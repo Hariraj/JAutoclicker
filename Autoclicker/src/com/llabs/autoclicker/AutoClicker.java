68cee23df25f8cdeee944796807e5d815c406449
Hariraj Sundaravadivelu <hariraj.vadivelu@gmail.com>
2012-06-23 14:36:30 +0530

    Added License

--- Autoclicker/src/com/llabs/autoclicker/AutoClicker.java
+++ Autoclicker/src/com/llabs/autoclicker/AutoClicker.java
@@ -1,12 +1,27 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Autoclicker ----------------- Copyright 20012-2013 JAutoclicker
+ * 
+ * I always give it my best shot to make a program useful and solid, but remember
+ * that there is absolutely no warranty for using this program as stated in the
+ * following terms:
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 /*
  * AutoClicker.java
  *
- * Created on Dec 22, 2010, 11:11:58 AM
+ * Created on Dec 22, 2012, 11:11:58 AM
  */
 package com.llabs.autoclicker;
 
